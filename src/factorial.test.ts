61949ee345f284a3c1632a5b5f28701b9a400036
Evan Kaiser <733581+holotrek@users.noreply.github.com>
2022-04-28 00:59:40 -0400

    Removes unused and proves that method is mocked.

--- src/factorial.test.ts
+++ src/factorial.test.ts
@@ -1,15 +1,11 @@
 import * as f from './factorial';
-//import { multiply } from './multiply';
-
-// jest.mock("./multiply", () => ({
-//   multiply: jest.fn(),
-// }));
 
 describe("factorial tests", () => {
   test("multiply called n times", () => {
     const mock = jest.spyOn(f, "multiply");
-    mock.mockReturnValue(1);
-    f.factorial(5);
+    mock.mockReturnValue(1); // always return 1 from multiply to check mock
+    const result = f.factorial(5);
     expect(f.multiply).toHaveBeenCalledTimes(5);
+    expect(result).toBe(1); // proves method is actually mocked since it is not 120
   });
 });
